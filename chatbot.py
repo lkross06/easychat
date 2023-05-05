6e9e6458c0ae9aa95a51471e21bcc3442373cba0
Lucas Kalani Ross <lkross@Lucass-MacBook-Pro.local>
2023-05-05 13:12:08 -0700

    Fixed intent recognition functions. Cleaned dataset, removing noise and making responses more favorable. Made chatbot.py for final project

---
+++ chatbot.py
@@ -0,0 +1,1 @@
+#same as chatbot.ipynb, but everything is in a py file so it can be executed
---
+++ user.py
@@ -0,0 +1,11 @@
+import datetime
+
+class UserData:
+    def __init__(self):
+        self.name = "?";
+    
+    def setName(self, name):
+        self.name = name
+
+    def getName(self):
+        return self.name
